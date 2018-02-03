8139d3d81234d970660b34c5bd7e5385427a8688
Andreas Brekken <andreas@brekken.com>
2018-02-03 11:58:02 -0400

    Add Coinex

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ exports.createBitfinexClient = require('./bitfinex');
 exports.createBitmexClient = require('./bitmex');
 exports.createKrakenClient = require('./kraken');
 exports.createPoloniexClient = require('./poloniex');
+exports.createCoinexClient = require('./coinex');
