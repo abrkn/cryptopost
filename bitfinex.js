79ab0f8d138e967e31e8cd52919a3adc04b1331d
Andreas Brekken <andreas@brekken.com>
2018-05-14 07:33:27 +0200

    allow key rotation for bitfinex

--- bitfinex.js
+++ bitfinex.js
@@ -1,13 +1,15 @@
 // From https://bitfinex.readme.io/v1/docs/rest-auth
 const superagent = require('superagent-use')(require('superagent'));
 const crypto = require('crypto');
-const { get } = require('lodash');
+const assert = require('assert');
 
 superagent.use(require('superagent-verbose-errors'));
 
 const baseUrl = 'https://api.bitfinex.com';
 
-const createBitfinexClient = (apiKey, apiSecret) => {
+const createBitfinexClient = (...args) => {
+  const nextKeyPair = typeof args[0] === 'function' ? args[0] : () => args.slice(0, 2);
+
   return async (url, fields = {}) => {
     const nonce = Date.now().toString();
     const completeURL = baseUrl + url;
@@ -19,6 +21,10 @@ const createBitfinexClient = (apiKey, apiSecret) => {
     };
 
     const payload = new Buffer(JSON.stringify(body)).toString('base64');
+
+    const [apiKey, apiSecret] = nextKeyPair();
+    assert(apiKey);
+    assert(apiSecret);
 
     const signature = crypto
       .createHmac('sha384', apiSecret)
